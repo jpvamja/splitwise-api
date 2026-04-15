137e1d50571680ffecf1cdceb2b58c74c654c14a
jpvamja <jpvamja@gmail.com>
2026-04-15 16:50:15 +0530

    feat: initialize project with TypeScript, Express, and ESLint setup
    - Add package.json with dependencies and scripts for development
    - Create tsconfig.json for TypeScript configuration
    - Add initial app and server files

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettier
+    ]
+  }
+])
